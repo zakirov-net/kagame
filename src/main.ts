51b829caf09e5215d506cf270bd4cd32e4f73690
Lenar Zakirov <lenar@zakirov.net>
2025-05-03 18:32:52 +0300

    First commit

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import kaplay from "kaplay";
+
+const k = kaplay({
+    width: 640,
+    height: 480,
+    background: [0, 0, 0],
+    global: false,
+    canvas: document.querySelector("#game") as HTMLCanvasElement,
+});
+
+const { loadSprite, add, sprite, pos, area, body, onKeyDown } = k;
+
+async function loadSprites() {
+    await Promise.all([
+        loadSprite("player", "/sprites/knight.png"),
+    ]);
+}
+loadSprites();
+
+const player = add([
+    sprite("player"),
+    pos(100, 100),
+    area(),
+    body(),
+]);
+
+onKeyDown("left", () => player.move(-120, 0));
+onKeyDown("right", () => player.move(120, 0));
